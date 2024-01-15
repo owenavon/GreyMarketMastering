e1b7ac7c3bc4bba2d8ee93a3fac2bf95cf4f5732
owenavon <89999663+owenavon@users.noreply.github.com>
2024-01-15 18:03:55 -0500

    Created bottom section with links. Added more CD covers. Changed placeholder studio photos. Added text under FAQ section. Copied clients list and format from the old website. Tried different fonts; monospace headings and cursive sub-headings.

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 // Change album cover order upon page reload, and every 5 seconds.
 const getRandomNumber = (function() {
-    var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76];
     var current = [];
     function rand(n) {
         return (Math.random() * n)|0;
@@ -65,12 +65,12 @@ const images = document.querySelectorAll('#gallery img');
 
 getRandomImages = () => {
   const imagesNums = [];
-  for (let i = 1; i < 16; i++) {
+  for (let i = 1; i < 77; i++) {
     imagesNums.push(getRandomNumber());
   }
 
   images.forEach((img, index) => {
-      img.src = `./assets/images/portfolio/thumbnails/${imagesNums[index]}.jpg`
+      img.src = `./assets/images/portfolio/covers/${imagesNums[index]}.jpg`
   })
 }
 
